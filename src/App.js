15cbe1073de73dbb8c0b399ef9fd2409ff6145f9
ZHamburglar <kollinbrandenburg@gmail.com>
2018-08-03 01:33:20 -0500

    adding the sidebar nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Home from './routes/Home';
 
+import SideBar from './components/SideNav'
+
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <SideBar />
         <div>
           <main>
             <div>
