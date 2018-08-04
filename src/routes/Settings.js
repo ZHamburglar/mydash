f5b584cae5ac0d97689f6b23d1f6f9703bfe96f1
ZHamburglar <kollinbrandenburg@gmail.com>
2018-08-03 19:58:31 -0500

    switch added for later use

--- src/routes/Settings.js
+++ src/routes/Settings.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import Switch from "react-switch";
+
+
 
 class Settings extends Component {
+  constructor() {
+    super();
+    this.state = { checked: false };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(checked) {
+    this.setState({ checked });
+  }
+
   render() {
     return (
       <div>
@@ -10,6 +23,13 @@ class Settings extends Component {
           <p>Settings Page</p>
           <p>Settings Page</p>
           <p>Settings Page</p>
+          <label htmlFor="normal-switch">
+            <Switch
+              onChange={this.handleChange}
+              checked={this.state.checked}
+              id="normal-switch"
+            />
+          </label>
       </div>
     );
   }
